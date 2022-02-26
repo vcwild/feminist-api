5882d12651dc27c3bdbd7b2effb96ed184a7dde9
Victor Wildner <vcwildm@gmail.com>
2022-02-26 14:38:05 -0300

    feat: implement cli

--- includes/ada.h
+++ includes/ada.h
@@ -7,24 +7,42 @@ Copyright (c) 2022 Victor Wildner
 */
 #ifndef ADA_H
 #define ADA_H
+#define _GNU_SOURCE
+
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "fort.h"
 
-#define CLI_VERSION "0.0.2"
-#define MAX_LENGTH 256
-#define APPLICATION_CLI_HELP_MSG "\
-Usage: %s [OPTION]\n\nMandatory arguments:\n\
-  -c, --clear\t\tClear the server events log;\n\
-  -l, --list\t\tList the server available endpoints;\n\
-  -h, --help\t\tPrints out the help menu;\n\
-  -v, --version\t\tGet the current CLI version;\n"
-#define EVENT_LOG_FILENAME "./logs/events.csv"
-#define INVALID_OPTION_MSG "Invalid optio n. Use --help for more information.\n"
+#define CLI_VERSION "0.1.1"
 
-typedef enum
-{
+#define MAX_LENGTH 256
+
+#define APPLICATION_CLI_HELP_MSG "\
+Usage: %s [OPTION]\
+\n\n\
+Optional arguments:\n\
+  -c, --clear\t\t\tClear the server events log;\n\
+  -e, --endpoints\t\tShow the available endpoints;\n\
+  -h, --help\t\t\tPrints out the help menu;\n\
+  -l, --list, --logs\t\tList the server requests;\n\
+  -s, --show\t\t\tAlias to --endpoints;\n\
+  -v, --version\t\t\tGet the current CLI version.\n"
+
+#define HOST_NAME "localhost"
+#define HOST_PORT "8000"
+#define HOST_URL "http://" HOST_NAME ":" HOST_PORT
+#define AVAILABLE_ENDPOINTS_MSG "Available endpoints:\n"\
+"  " HOST_URL "/\n"\
+"  " HOST_URL "/api\n"\
+"  " HOST_URL "/mock\n"
+
+#define EVENT_LOG_FILENAME "./logs/events.csv"
+
+#define INVALID_OPTION_MSG "Invalid option. Use --help for more information.\n"
+
+typedef enum s_req_type{
   GET_ROOT,
   GET_API,
   GET_MOCK,
--- sources/cli.c
+++ sources/cli.c
@@ -56,26 +56,53 @@ static void prepare_table_styles(ft_table_t *table) {
 }
 
 /**
+ * @brief A helper function to render table lines in the CLI.
+ *
+ * @param table The table to render.
+ * @param logs The logs to get info from.
+ * @param e_req The request type to filter.
+ * @param method The HTTP method to display.
+ * @param path The url PATH to display.
+ * @param msg The about message.
+ */
+static int table_render_line(
+  ft_table_t *table,
+  int *logs,
+  t_req_type e_req,
+  char *method,
+  char *path,
+  char *msg) {
+  char tmp[10];
+
+  if (logs[e_req]) {
+    sprintf(tmp, "%2d", logs[e_req]);
+    ft_write_ln(table, method, path, tmp, msg);
+    return 1;
+  }
+  return 0;
+}
+
+/**
  * @brief Generates a table with the server events log.
  *
  * @param table The table reference to populate.
  * @param logs The logs counted by the server.
  */
 static void populate_table(ft_table_t *table, int *logs) {
-  char tmp[10];
+  int nlines = 0;
   ft_write_ln(table, "HTTP Method", "PATH", "Requests", "About");
-  sprintf(tmp, "%2d", logs[GET_ROOT]);
-  ft_write_ln(table, "GET", "/", tmp, "GET a random quote");
-  sprintf(tmp, "%2d", logs[GET_API]);
-  ft_write_ln(table, "GET", "/api", tmp, "GET a random quote in JSON format");
-  sprintf(tmp, "%2d", logs[GET_MOCK]);
-  ft_write_ln(table, "GET", "/mock", tmp, "GET information about /mock");
-  sprintf(tmp, "%2d", logs[POST_MOCK]);
-  ft_write_ln(table, "POST", "/mock", tmp, "POST an update to /mock");
-  sprintf(tmp, "%2d", logs[PUT_MOCK]);
-  ft_write_ln(table, "PUT", "/mock", tmp, "PUT new data in /mock");
-  sprintf(tmp, "%2d", logs[DELETE_MOCK]);
-  ft_write_ln(table, "DELETE", "/mock", tmp, "DELETE data from /mock");
+
+  nlines += table_render_line(table, logs, GET_ROOT, "GET", "/", "GET a random quote");
+  nlines += table_render_line(table, logs, GET_API, "GET", "/api", "GET a random quote in JSON format");
+  nlines += table_render_line(table, logs, GET_MOCK, "GET", "/mock", "GET information about /mock");
+  nlines += table_render_line(table, logs, POST_MOCK, "POST", "/mock", "POST an update to /mock");
+  nlines += table_render_line(table, logs, PUT_MOCK, "PUT", "/mock", "PUT an update to /mock");
+  nlines += table_render_line(table, logs, DELETE_MOCK, "DELETE", "/mock", "DELETE data from /mock");
+
+  if (!nlines) {
+    ft_write_ln(table, "None", "None", "None", "None");
+  }
+
   ft_set_cell_prop(
     table, FT_ANY_ROW, FT_ANY_COLUMN,
     FT_CPROP_TEXT_ALIGN, FT_ALIGNED_CENTER
@@ -101,7 +128,7 @@ static void draw_table(int *logs) {
  * @param filename The path of the file to be read.
  * @return int Returns `0` on success.
  */
-static int handle_logs(char *filename)
+static int handle_list(char *filename)
 {
   int logs[ENUM_LENGTH];
   logs[GET_ROOT] = count_word_matches(filename, "GET,ROOT");
@@ -132,16 +159,57 @@ static int clear_event_log(char *filename)
   return 0;
 }
 
+static int handle_log_print(ft_table_t *table, char *filename)
+{
+    FILE *fp;
+    char *args[MAX_LENGTH];
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    if ((fp = fopen(filename, "r")) == NULL)
+        exit(EXIT_FAILURE);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+      args[0] = strtok(line, ",");
+      args[1] = strtok(NULL, ",");
+      args[2] = strtok(NULL, "\n");
+      ft_write_ln(table, args[0], args[1], args[2]);
+    }
+    fclose(fp);
+    if (line)
+        free(line);
+    return 0;
+}
+
+static int handle_logs(char *filename) {
+  ft_table_t *table = ft_create_table();
+  prepare_table_styles(table);
+  handle_log_print(table, filename);
+  ft_set_cell_prop(
+    table, FT_ANY_ROW, FT_ANY_COLUMN,
+    FT_CPROP_TEXT_ALIGN, FT_ALIGNED_CENTER
+  );
+  printf("%s\n", ft_to_string(table));
+  ft_destroy_table(table);
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
   if (argc != 2 ||
-    strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)
+      strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)
     return fprintf(stdout, APPLICATION_CLI_HELP_MSG, argv[0]);
   if (strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-v") == 0)
     return fprintf(stdout, "%s v%s\n", argv[0], CLI_VERSION);
   if (strcmp(argv[1], "--list") == 0 || strcmp(argv[1], "-l") == 0)
+    return handle_list(EVENT_LOG_FILENAME);
+  if (strcmp(argv[1], "--logs") == 0)
     return handle_logs(EVENT_LOG_FILENAME);
-  if (strcmp(argv[1], "--clear") != 0 || strcmp(argv[1], "-c") != 0)
+  if (strcmp(argv[1], "--clear") == 0 || strcmp(argv[1], "-c") == 0)
     return clear_event_log(EVENT_LOG_FILENAME);
+  if (strcmp(argv[1], "--show") == 0 || strcmp(argv[1], "-s") == 0 ||
+      strcmp(argv[1], "--endpoints") == 0 || strcmp(argv[1], "-e") == 0)
+    return fprintf(stdout, AVAILABLE_ENDPOINTS_MSG);
   fprintf(stderr, INVALID_OPTION_MSG);
   return -1;
 }
